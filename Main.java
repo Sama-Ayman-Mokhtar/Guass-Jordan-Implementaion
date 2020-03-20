665d9d5ec3d9045017646f225398f09976456927
Sama Ayman <54854067+Sama-Ayman-Mokhtar@users.noreply.github.com>
2020-03-20 09:22:29 -1000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//This Code Won't Work If Any Of The Terms Equals Zero
+        Ioput.fillArray();
+        Solve.loopCols();
+        Ioput.printResult();
+    }
+}
